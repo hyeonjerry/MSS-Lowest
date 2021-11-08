f85a914140b8a569d74f05148ed2614d9f617331
Hyeonjae-K <rnjsguswo012@hufs.ac.kr>
2021-11-08 21:15:50 +0900

    FEAT - 모델 생성

--- lowest/models.py
+++ lowest/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class Brands(models.Model):
+    path = models.CharField(max_length=40, primary_key=True)
+    ko_name = models.CharField(max_length=30)
+    en_name = models.CharField(max_length=40)
+    img = models.CharField(max_length=255)
+    created_date = models.DateTimeField()
+
+
+class Products_History(models.Model):
+    product = models.ForeignKey('Products', on_delete=models.CASCADE)
+    price = models.IntegerField()
+    created_date = models.DateTimeField()
+
+
+class Products(models.Model):
+    product_id = models.BigIntegerField(primary_key=True)
+    brand = models.ForeignKey(Brands, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    lowest = models.ForeignKey(
+        'Products_History', on_delete=models.CASCADE, related_name='lowest')
+    current = models.ForeignKey(
+        'Products_History', on_delete=models.CASCADE, related_name='current')
+    highest = models.ForeignKey(
+        'Products_History', on_delete=models.CASCADE, related_name='highest')
+    img = models.CharField(max_length=255)
