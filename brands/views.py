fccbea8854a68af42d668c48af491e04f16fb72f
Hyeonjae-K <href.hjk@gmail.com>
2022-07-02 17:42:20 +0100

    FEAT - 페이지 기능 구현

--- brands/views.py
+++ brands/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 
 from brands.models import Brand
 
 
 def brandsList(request):
+    page = request.GET.get('page', '1')
     brands = Brand.objects.order_by('en_name')
-    context = {'brands': brands}
+    paginator = Paginator(brands, 15)
+    page_obj = paginator.get_page(page)
+    context = {'brands': page_obj, 'page': page}
     return render(request, 'brands/brands_list.html', context)
