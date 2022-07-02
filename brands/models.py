a38191cbe36324579fcb1cf69cc7ce79a591297c
Hyeonjae-K <href.hjk@gmail.com>
2022-07-02 11:08:10 +0100

    ADD - Brand 모델 추가

--- brands/models.py
+++ brands/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Brand(models.Model):
+    ko_name = models.CharField(max_length=35)
+    en_name = models.CharField(max_length=50)
+    url = models.URLField(unique=True)
+
+    def __str__(self):
+        return f"{self.ko_name}({self.en_name})"
