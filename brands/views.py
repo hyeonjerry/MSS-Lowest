aad9593a361f33ec6dd6beee0c1da57c40953045
Hyeonjae-K <href.hjk@gmail.com>
2022-07-02 11:36:29 +0100

    FIX - context 오류 수정

--- brands/views.py
+++ brands/views.py
@@ -5,4 +5,5 @@ from brands.models import Brand
 
 def brandsList(request):
     brands = Brand.objects.order_by('en_name')
+    context = {'brands': brands}
     return render(request, 'brands/brands_list.html', context)
