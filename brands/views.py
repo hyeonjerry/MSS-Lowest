004b6e3881b78a9097e1a17a0eb058c7a238d891
Hyeonjae-K <href.hjk@gmail.com>
2022-07-07 17:37:52 +0100

    FEAT - 페이징 기능 구현 및 paginator 모듈화

--- brands/views.py
+++ brands/views.py
@@ -9,12 +9,14 @@ def brandsList(request):
     brands = Brand.objects.order_by('en_name')
     paginator = Paginator(brands, 15)
     page_obj = paginator.get_page(page)
-    context = {'brands': page_obj, 'page': page}
+    context = {'page_objs': page_obj, 'page': page}
     return render(request, 'brands/brands_list.html', context)
 
 
 def brandDetail(request, brand_name):
+    page = request.GET.get('page', '1')
     brand = Brand.objects.get(suffix=brand_name)
     products = brand.brand_product.all()
-    context = {'brand': brand, 'products': products}
+    products = Paginator(products, 15).get_page(page)
+    context = {'brand': brand, 'page_objs': products, 'page': page}
     return render(request, 'brands/brand_detail.html', context)
