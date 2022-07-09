83c03ed03432a3dc21c36219d6003d20c0840009
Hyeonjae-K <href.hjk@gmail.com>
2022-07-09 15:07:05 +0100

    FEAT - 상품 목록 페이지 구현

--- products/views.py
+++ products/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 
-# Create your views here.
+from products.models import Product, ProductHistory
+
+
+def productsList(request):
+    page = request.GET.get('page', '1')
+    products = Product.objects.order_by('-updated_date')
+    paginator = Paginator(products, 15)
+    page_obj = paginator.get_page(page)
+    context = {'page_objs': page_obj, 'page': page}
+    return render(request, 'products/products_list.html', context)
