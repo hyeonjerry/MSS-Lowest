876cc9e5bae006fffbc3c33b8cd426951be950e0
Hyeonjae-K <href.hjk@gmail.com>
2022-07-10 05:31:00 +0100

    FEAT - 상품 detail 페이지 구현

--- products/urls.py
+++ products/urls.py
@@ -6,4 +6,5 @@ app_name = 'products'
 
 urlpatterns = [
     path('', views.productsList, name='productsList'),
+    path('<product_id>/', views.productDetail, name='productDetail'),
 ]
--- products/views.py
+++ products/views.py
@@ -11,3 +11,10 @@ def productsList(request):
     page_obj = paginator.get_page(page)
     context = {'page_objs': page_obj, 'page': page}
     return render(request, 'products/products_list.html', context)
+
+
+def productDetail(request, product_id):
+    product = Product.objects.get(id=product_id)
+    histories = product.product_producthistory.all().order_by('-created_date')
+    context = {'product': product, 'histories': histories}
+    return render(request, 'products/product_detail.html', context)
