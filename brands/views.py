fac5dead33a3bf8cfed7cace604d1d9738c71098
Hyeonjae-K <href.hjk@gmail.com>
2022-07-10 06:44:17 +0100

    FEAT - 앱 별로 검색 기능 구현

--- brands/views.py
+++ brands/views.py
@@ -1,16 +1,22 @@
 from django.shortcuts import render
 from django.core.paginator import Paginator
+from django.db.models import Q
 
 from brands.models import Brand
 
 
 def brandsList(request):
+    kw = request.GET.get('kw', '')
     page = request.GET.get('page', '1')
-    brands = Brand.objects.order_by('en_name')
+    if kw:
+        brands = Brand.objects.filter(Q(ko_name__icontains=kw) | Q(
+            en_name__icontains=kw)).distinct().order_by('en_name')
+    else:
+        brands = Brand.objects.order_by('en_name')
     paginator = Paginator(brands, 15)
     page_obj = paginator.get_page(page)
-    context = {'page_objs': page_obj, 'page': page}
-    return render(request, 'brands/brands_list.html', context)
+    context = {'page_objs': page_obj, 'page': page, 'kw': kw}
+    return render(request, 'brands/list.html', context)
 
 
 def brandDetail(request, brand_name):
@@ -19,4 +25,4 @@ def brandDetail(request, brand_name):
     products = brand.brand_product.all()
     products = Paginator(products, 15).get_page(page)
     context = {'brand': brand, 'page_objs': products, 'page': page}
-    return render(request, 'brands/brand_detail.html', context)
+    return render(request, 'brands/detail.html', context)
--- products/views.py
+++ products/views.py
@@ -10,17 +10,17 @@ def productsList(request):
     page = request.GET.get('page', '1')
     if kw:
         products = Product.objects.filter(
-            Q(name__icontains=kw) | Q(brand__ko_name__icontains=kw) | Q(brand__en_name__icontains=kw)).distinct().order_by('updated_date')
+            Q(name__icontains=kw)).distinct().order_by('updated_date')
     else:
         products = Product.objects.order_by('-updated_date')
     paginator = Paginator(products, 15)
     page_obj = paginator.get_page(page)
     context = {'page_objs': page_obj, 'page': page, 'kw': kw}
-    return render(request, 'products/products_list.html', context)
+    return render(request, 'products/list.html', context)
 
 
 def productDetail(request, product_id):
     product = Product.objects.get(id=product_id)
     histories = product.product_producthistory.all().order_by('-created_date')
     context = {'product': product, 'histories': histories}
-    return render(request, 'products/product_detail.html', context)
+    return render(request, 'products/detail.html', context)
