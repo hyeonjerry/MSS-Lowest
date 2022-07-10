db19192c5d2456e6325f3538ac536a4f39a5312a
Hyeonjae-K <href.hjk@gmail.com>
2022-07-10 06:04:18 +0100

    FEAT - 검색 기능 구현

--- products/views.py
+++ products/views.py
@@ -1,15 +1,21 @@
 from django.shortcuts import render
 from django.core.paginator import Paginator
+from django.db.models import Q
 
-from products.models import Product, ProductHistory
+from products.models import Product
 
 
 def productsList(request):
+    kw = request.GET.get('kw', '')
     page = request.GET.get('page', '1')
-    products = Product.objects.order_by('-updated_date')
+    if kw:
+        products = Product.objects.filter(
+            Q(name__icontains=kw) | Q(brand__ko_name__icontains=kw) | Q(brand__en_name__icontains=kw)).distinct().order_by('updated_date')
+    else:
+        products = Product.objects.order_by('-updated_date')
     paginator = Paginator(products, 15)
     page_obj = paginator.get_page(page)
-    context = {'page_objs': page_obj, 'page': page}
+    context = {'page_objs': page_obj, 'page': page, 'kw': kw}
     return render(request, 'products/products_list.html', context)
 
 
