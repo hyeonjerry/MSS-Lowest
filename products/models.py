00a20825da65c23f88184f3b0c29a683310a4594
Hyeonjae-K <href.hjk@gmail.com>
2022-07-13 04:46:04 +0100

    DEL - Category 테이블 및 category 컬럼 삭제

--- products/models.py
+++ products/models.py
@@ -3,18 +3,6 @@ from django.db import models
 from brands.models import Brand
 
 
-class Category(models.Model):
-    parent = models.ForeignKey('self', on_delete=models.CASCADE,
-                               null=True, blank=True, related_name='parent_category')
-    name = models.CharField(max_length=25, unique=True)
-    url = models.URLField(unique=True)
-
-    def __str__(self):
-        if self.parent:
-            return f"{self.parent.name} > {self.name}"
-        return self.name
-
-
 class Product(models.Model):
     name = models.CharField(max_length=100)
     brand = models.ForeignKey(
@@ -26,8 +14,6 @@ class Product(models.Model):
         'ProductHistory', on_delete=models.CASCADE, related_name='lowest_product', null=True, blank=True)
     highest_price = models.ForeignKey(
         'ProductHistory', on_delete=models.CASCADE, related_name='highest_product', null=True, blank=True)
-    category = models.ForeignKey(
-        Category, on_delete=models.CASCADE, related_name='category_product', null=True, blank=True)
     thumbnail = models.URLField()
     url = models.URLField(unique=True)
     created_date = models.DateTimeField(auto_now_add=True)
