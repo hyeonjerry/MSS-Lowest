78ac949acad4f7aa97acc41e748467a605acf090
Hyeonjae-K <href.hjk@gmail.com>
2022-07-13 04:45:42 +0100

    DEL - suffix 컬럼 삭제

--- brands/models.py
+++ brands/models.py
@@ -4,8 +4,6 @@ from django.db import models
 class Brand(models.Model):
     ko_name = models.CharField(max_length=35)
     en_name = models.CharField(max_length=50)
-    suffix = models.CharField(
-        max_length=50, unique=True)
     url = models.URLField(unique=True)
 
     def __str__(self):
