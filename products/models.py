8c768e2c641349251e0735a215ab9dd49e42ae08
Hyeonjae-K <href.hjk@gmail.com>
2022-08-21 03:52:21 +0900

    FIX - 필드명 변경 및 id 필드 추가

--- products/models.py
+++ products/models.py
@@ -4,17 +4,18 @@ from brands.models import Brand
 
 
 class Product(models.Model):
+    id = models.PositiveBigIntegerField(primary_key=True)
     name = models.CharField(max_length=100)
     brand = models.ForeignKey(
         Brand, on_delete=models.CASCADE, related_name='brand_product')
-    requested_price = models.IntegerField()
+    retail_price = models.IntegerField()
     last_price = models.ForeignKey(
         'ProductHistory', on_delete=models.CASCADE, related_name='last_product', null=True, blank=True)
     lowest_price = models.ForeignKey(
         'ProductHistory', on_delete=models.CASCADE, related_name='lowest_product', null=True, blank=True)
     highest_price = models.ForeignKey(
         'ProductHistory', on_delete=models.CASCADE, related_name='highest_product', null=True, blank=True)
-    thumbnail = models.URLField()
+    image = models.URLField()
     url = models.URLField(unique=True)
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
@@ -25,7 +26,7 @@ class Product(models.Model):
 
 class ProductHistory(models.Model):
     product = models.ForeignKey(
-        Product, on_delete=models.CASCADE, related_name='product_producthistory')
+        Product, on_delete=models.CASCADE, related_name='producthistory')
     price = models.IntegerField()
     created_date = models.DateTimeField(auto_now=True)
 
