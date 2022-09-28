a94e5410bb428d23e0b294ed42a0d47d9fc66f86
Hyeonjae-K <href.hjk@gmail.com>
2022-09-28 12:06:39 +0900

    Project Reinitialize

--- brands/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from brands.models import Brand
-
-
-class BrandSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Brand
-        fields = ['id', 'ko_name', 'en_name', 'url']
