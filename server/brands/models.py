3bcff2825567ede23c73743c3df42ec96763bcdc
Hyeonjae-K <href.hjk@gmail.com>
2022-09-30 00:02:08 +0900

    Add created date

--- server/brands/models.py
+++ server/brands/models.py
@@ -5,3 +5,4 @@ class Brand(models.Model):
     ko_name = models.CharField(max_length=50)
     en_name = models.CharField(max_length=100)
     url = models.URLField(max_length=250)
+    created_date = models.DateTimeField(auto_now_add=True)
